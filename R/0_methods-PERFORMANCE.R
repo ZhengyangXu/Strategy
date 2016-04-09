5d434523a944cb9999f825d18a956b7660b547db
quants-ch <jb@quants.ch>
2016-04-09 20:21:00 +0200

    made changes

--- R/0_methods-PERFORMANCE.R
+++ R/0_methods-PERFORMANCE.R
@@ -351,7 +351,6 @@ setMethod(f = "VaR",
               scaling.periods <- .annFactor(L)
             
             # validations
-            print(scaling.periods)
             if (length(scaling.periods) == 1) scaling.periods <- rep(scaling.periods, ncol(L))
             if (length(scaling.periods) != ncol(L)) stop("Please provide scaling periods as numeric vector with length 1 or the same number of portfolio assets.")
             
