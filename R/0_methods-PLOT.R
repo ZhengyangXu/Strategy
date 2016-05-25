a5c632d1b97adc0b3b4cb9d02e34e2c4ba56e56f
quants-ch <jb@quants.ch>
2016-05-25 12:30:08 +0200

    made changes

--- R/0_methods-PLOT.R
+++ R/0_methods-PLOT.R
@@ -409,3 +409,64 @@ setMethod(f = "plotDrawdowns",
               return(dd)
         } #method function
 )
+
+setGeneric(name = "plotWeights",
+           def = function(object, from=NULL, until=NULL, ...) {
+             standardGeneric("plotWeights")
+           }
+)
+
+#' @export
+#' @name plotWeights
+#' @aliases plotWeights
+#' @title Plot Strategy Weights
+#' @description Plots the weights of the portfolio of an object of class \code{Strategy}.
+#' @usage plotWeights(object, from=NULL, until=NULL, ...)
+#' @param object An object of class \code{Strategy}.
+#' @param from The date in character format \code{"yyyy-MM-dd"} or as date-object from which weights shall be plotted. If \code{NULL}, the start date of the weights is used.
+#' @param until The date in character format \code{"yyyy-MM-dd"} or as date-object until which weights shall be plotted. If \code{NULL}, the end date of the performances is used.
+#' @param ... Currently not active.
+#' @examples
+#' ##Not run:
+#' # Plot MA(200)-Strategy weights
+#' plotWeights(myStrat.MA)
+#'
+#' ##End(Not run)
+setMethod(f = "plotWeights",
+          signature = "Strategy",
+          definition = function(object, from, until, ...) {
+  
+  par.mar <- par()$mar
+  
+  # data
+  weights <- abs(getWeights(object))
+  signals <- getSignals(object)
+  col <- rainbow(ncol(weights))
+  
+  mar <- c(5, 4, 4, 2) + 0.1
+  
+  # plot
+  layout(matrix(c(1,2),ncol=2),widths=c(0.8,0.2))
+  par(mar=c(mar[1:3],0))
+  # weights
+  plot(weights[,1], ylim=c(0,max(rowSums(weights))), type="n", las=2, minor.ticks=F, axes=FALSE, yaxs="i")
+  axis(1, at=.index(weights)[axTicksByTime(weights)], labels=names(axTicksByTime(weights)), las=2)
+  axis(2, las=2)
+  par(new=TRUE)
+  barplot(weights, col = col, space = 0, ylab = ""
+          , border = NA, axes=FALSE, names.arg="", yaxs="i", ylim=c(0,max(rowSums(weights))))
+  graphics::box()
+  legendtext <- colnames(weights)
+  if (is.null(legendtext)) {
+    for (i in 1:dim[2]) {
+      legendtext[i] = paste("Asset", i, sep = " ")
+    }
+  }
+  # legend
+  par(mar=c(mar[1], 0, mar[3],0))
+  plot(0,0,type="n",axes=FALSE,xlab="",ylab="",main="")
+  legend("center", legend = legendtext, bty = "n", cex = 0.7, fill = col)
+  
+  layout(1)
+  par(mar=par.mar)
+})
